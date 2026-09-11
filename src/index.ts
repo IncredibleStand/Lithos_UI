8709034949fcc8c3ef8b19ece2e783f0293bcfe6
madhav9757 <madhavsemwal9@gmail.com>
2026-09-11 15:46:41 +0530

    changed the exports from tooltip/Tooltip.tsx too ---> ui/Tooltip.ts

--- src/index.ts
+++ src/index.ts
@@ -26,10 +26,7 @@ export * from './components/ui/Select'
 export * from './components/ui/Tabs'
 export * from './components/ui/Toast'
 export * from './components/ui/Toggle'
-export * from './components/ui/tooltip/Tooltip'
-export * from './components/ui/tooltip/TooltipContent'
-export * from './components/ui/tooltip/TooltipTrigger'
-export * from './components/ui/tooltip/useTooltip'
+export * from './components/ui/Tooltip'
 
 // Hooks
 export * from './core/hooks/useFocusTrap'
